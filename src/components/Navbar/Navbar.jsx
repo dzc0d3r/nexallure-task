1e1c831dcd47f45ab93c623e182c10c9de4e5856
Walid Lamraoui <m4dcod3r@protonmail.com>
2024-01-26 16:20:19 +0100

    fix(navbar): fix missing suspense boundary

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,33 +6,39 @@ import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { Suspense } from "react"
 
-const Navbar = () => {
+const Nav = () => {
   const pathname = usePathname()
 
   const { user, setOpenWallet } = useAuth()
   return (
-    <Suspense>
-      <nav
-        className={` sticky top-0 z-40 min-h-20 w-full items-center p-3 md:p-5 ${pathname == "/wallet" ? "bg-slate-200" : "bg-white"}`}
-      >
-        <div className="container mx-auto flex max-w-7xl items-center justify-between">
-          <div className="p-5">
-            <h1 className="h1">
-              <Link href="/">Marketplace.</Link>
-            </h1>
-          </div>
-
-          {user ? (
-            <Button variant="primary" onClick={() => setOpenWallet(true)}>
-              account
-            </Button>
-          ) : (
-            <Link href="wallet">
-              <Button>connect wallet</Button>
-            </Link>
-          )}
+    <nav
+      className={` sticky top-0 z-40 min-h-20 w-full items-center p-3 md:p-5 ${pathname == "/wallet" ? "bg-slate-200" : "bg-white"}`}
+    >
+      <div className="container mx-auto flex max-w-7xl items-center justify-between">
+        <div className="p-5">
+          <h1 className="h1">
+            <Link href="/">Marketplace.</Link>
+          </h1>
         </div>
-      </nav>
+
+        {user ? (
+          <Button variant="primary" onClick={() => setOpenWallet(true)}>
+            account
+          </Button>
+        ) : (
+          <Link href="wallet">
+            <Button>connect wallet</Button>
+          </Link>
+        )}
+      </div>
+    </nav>
+  )
+}
+
+const Navbar = () => {
+  return (
+    <Suspense>
+      <Nav />
     </Suspense>
   )
 }
