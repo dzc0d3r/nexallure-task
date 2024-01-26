6fc910d16cf789fc89c62f4792f248db4cd365bc
Walid Lamraoui <m4dcod3r@protonmail.com>
2024-01-26 17:05:49 +0100

    fix(deploy): fix broken deployment

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import { CollectionCard } from "@/components/Collection"
 import { Hero } from "@/components/Hero"
 
 export default async function Home() {
-  const data = await fetch(`${process.env.API_URL}/collections`)
+  const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/collections`)
   const collections = await data.json()
 
   return (
